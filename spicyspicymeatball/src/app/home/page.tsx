faba42fb9fefacb74faf87e577cd84f4bf24cbbf
Reid T. <2411400095@k12.hi.us>
2025-04-14 09:25:34 -1000

    committing again

--- spicyspicymeatball/src/app/home/page.tsx
+++ spicyspicymeatball/src/app/home/page.tsx
@@ -1,12 +1,48 @@
 import Link from "next/link";
 import AddButton from "./addbutton";
+import { db } from "~/server/db";
+import { posts } from "~/server/db/schema";
 
-export default function HomePage() {
-  return (
-    <main className="bg-[#FA946E]">
-        <h1 className="bg-[#FF5032] font-serif text-7xl flex justify-center"> = Popular Recipes Now! = </h1>
-        <AddButton>
-        </AddButton>
-    </main>
-  );
-};
+export default async function HomePage() {
+  const data = await db.query.posts.findMany({
+    columns: {
+      recipename: true,
+      ingredients: true,
+      directions: true,
+    },
+  })
+  
+  
+  return (<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <h1 className="bg-[#FF5032] font-serif text-7xl flex justify-center"> = Popular Recipes Now! = </h1>
+    {data.map((data,index) => (
+      <div key={index} className="card w-96 bg-base-100 shadow-sm">
+        <div className="card-body">
+          <h1 className="card-title">How to Make: {data.recipename}</h1>
+          <h2>Ingredients:</h2>
+          <p>{data.ingredients}</p>
+          <h2>Directions:</h2>
+          <p>{data.directions}</p>
+          <div className="card-actions">
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+    );
+  }
+
+
+  //this commented code below is to show you how to straight up map something as output on your page, unformatted
+  //this is the concept that is used below along with the daisyUI cards to output all of your data in your schema / array
+    /*return (<div>
+        {data.map((data,index) => (
+          <div key={index}>{data.title} - {data.content}</div>
+        ))}
+      </div>)*/
+
+
+  //this is how to format it with daisyUI cards, you can change the classes to whatever you want, just remember to use tailwind classes
+  //this is a grid layout, you can change the grid-cols-1 to whatever you want, just remember to use tailwind classes
+  //the biggest change done here is basically you need to wrap all of the  tailwind card stuff with the map function
+  // This forces the cards to be generated for each item in the arrayinstead of just one card for the whole array.
--- spicyspicymeatball/src/app/quitbutton.tsx
+++ spicyspicymeatball/src/app/quitbutton.tsx
@@ -4,12 +4,11 @@ const QuitButton: React.FC = () => {
     return (
         <main className="p-4">
         <div className="bg-white rounded-lg shadow-md w-full max-w-md">
-        <Link href="/home">
-            <button>Quit</button>
-        </Link>
+        <Link href="/home"/>
+            <button>Quit</button>  
         </div>
         </main>
     );
 };
 
-export default QuitButton;
+export default QuitButton;   
